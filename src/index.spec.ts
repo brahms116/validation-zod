43a8565f7f7d1f0b358af01c742e9033c0fb4937
David Kwong <20544dk@gmail.com>
2022-09-01 07:50:07 +0000

    main - setup

---
+++ src/index.spec.ts
@@ -0,0 +1,5 @@
+describe("index", () => {
+  it("should work", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import type { ZodError, z } from "zod";
+import { Opt, WhoopsError, Ok, Err, Whoops, RsResult } from "std-lib";
+
+export function ValidateZod<T extends Zod.ZodTypeAny, ErrorType extends {}>(
+  schema: T,
+  raw: any,
+  validationErrKind: ErrorType
+): RsResult<z.infer<typeof schema>, WhoopsError<ErrorType>> {
+  const result = schema.safeParse(raw);
+
+  if (result.success) return Ok(result.data);
+
+  const err = Whoops(validationErrKind)
+    .ctx("At zod validation.")
+    .reason(formatZodMsg(result.error))
+    .build();
+
+  return Err(err);
+}
+
+function handlePaths(paths: (string | number)[]) {
+  let res = "";
+  for (let i = 0; i < paths.length; i++) {
+    const el = paths[i];
+    if (typeof el === "string") {
+      res = res + el;
+    }
+    if (typeof el === "number") {
+      res = res + `[${el}]`;
+    }
+    if (i < paths.length - 1) {
+      res = res + ".";
+    }
+  }
+  return res;
+}
+
+function formatZodMsg(err: ZodError): string {
+  const issueOpt = Opt(err.issues[0]);
+
+  if (issueOpt.isNone()) {
+    return "Empty Zod Error";
+  }
+  const issue = issueOpt.unwrap();
+  let field = "";
+  if (issue.path.length) {
+    field = `At ${handlePaths(issue.path)}, `;
+  }
+  return `${field} ${issue.message}`;
+}
